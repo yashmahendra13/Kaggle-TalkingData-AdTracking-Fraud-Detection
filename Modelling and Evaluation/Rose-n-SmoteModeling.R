44049b7bbbd3c00ca119d1b49681f2cda1c968f7
Yash Mahendra <yashmahendra13@gmail.com>
2018-05-15 01:08:05 -0400

    Third Update

--- Modelling and Evaluation/Rose-n-SmoteModeling.R
+++ Modelling and Evaluation/Rose-n-SmoteModeling.R
@@ -26,11 +26,15 @@ featuresDF$is_attributed <- as.factor(featuresDF$is_attributed)
 testDf <- featuresDF[c(rep(FALSE,4),TRUE), ]
 trainingDf <- featuresDF[c(rep(TRUE,4),FALSE), ]
 
+
+
+#Random Forest
 rf <- randomForest(is_attributed~.,data=trainingDf,importance=TRUE,ntree=200)
 importance(rf)
 varImpPlot(rf)
 prediction("Random Forest", rf, testDf, 29)  #29 is col number of is_attributed
 
+#C4.5 Decision Tree
 c45 <- J48(is_attributed~., data=trainingDf)
 #summary(c45)
 prediction("C4.5", c45, testDf, 29) #29 is col number of is_attributed
@@ -46,11 +50,35 @@ featuresDF$is_attributed <- as.factor(featuresDF$is_attributed)
 testDf <- featuresDF[c(rep(FALSE,4),TRUE), ]
 trainingDf <- featuresDF[c(rep(TRUE,4),FALSE), ]
 
+#Random Forest
 rf <- randomForest(is_attributed~.,data=trainingDf,importance=TRUE,ntree=200)
 importance(rf)
 varImpPlot(rf)
 prediction("Random Forest", rf, testDf, 3) #3 is col number of is_attributed
 
+#C4.5 Decision Tree
 c45 <- J48(is_attributed~., data=trainingDf)
 #summary(c45)
 prediction("C4.5", c45, testDf, 3)  #3 is col number of is_attributed
+
+##Neural Network
+m <- model.matrix( 
+  ~is_attributed+min1_mean+min1_max+min1_var+min5_mean+min5_max+min5_var+hr1_mean+hr1_max+hr1_var+hr3_mean+hr3_max+hr3_var+maxClicks+noOfIPs+clickToIpRatio+clickVariance+entropy+maxClicksDevice+noOfDevices+clickToDeviceRatio+clickVarianceDevice+entropyDevice+maxClicksChannel+noOfChannel+clickToChannelRatio+clickVarianceChannel+entropyChannel, 
+  data =trainingDf 
+)
+net_bc2 <-neuralnet(is_attributed1~min1_mean+min1_max+min1_var+min5_mean+min5_max+min5_var+hr1_mean+hr1_max+hr1_var+hr3_mean+hr3_max+hr3_var+maxClicks+noOfIPs+clickToIpRatio+clickVariance+entropy+maxClicksDevice+noOfDevices+clickToDeviceRatio+clickVarianceDevice+entropyDevice+maxClicksChannel+noOfChannel+clickToChannelRatio+clickVarianceChannel+entropyChannel
+                      ,m, hidden=5, threshold=0.01)
+
+#Plot the neural network
+plot(net_bc2)
+
+net_bc2_results <-compute(net_bc2, testDf[,c(cols)])
+ANN=as.numeric(net_bc2_results$net.result)
+ANN_round<-round(ANN)
+
+table(Actual=test_sampled$is_attributed,ANN_round)
+
+wrong<- (testDf$is_attributed!=ANN_round)
+rate<-sum(wrong)/length(wrong)
+rate_FN<-2/length(wrong)
+rate_FN
